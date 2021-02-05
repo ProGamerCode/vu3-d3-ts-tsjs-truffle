56e9a262825c48b9e785a10e4fbd00118c885786
ProGamerCode <progamercode@outlook.com>
2021-02-05 03:42:06 -0800

    eslint configg3d

---
+++ src/components/dtrizzy.vue
@@ -0,0 +1,12 @@
+<template> 
+ <q-page>
+<div{{bbb}}></div>
+</q-page>
+</template>
+
+<script src="https://d3js.org/d3.v6.min.js>let :bbb= d3.select("body")..style("background-color", "black");
+
+export default {
+  name: 'dtrizzy'}
+</script>
+
